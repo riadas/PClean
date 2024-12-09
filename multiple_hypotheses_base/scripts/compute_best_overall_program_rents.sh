f7c2b7126fcfba84392d5716652be1d05436b9fc
riadas <riadas@mit.edu>
2024-12-08 22:07:07 -0500

    oops

--- multiple_hypotheses_base/scripts/compute_best_overall_program_rents.sh
+++ multiple_hypotheses_base/scripts/compute_best_overall_program_rents.sh
@@ -9,12 +9,14 @@ do
     for benchmark in ${benchmarks[@]}; 
     do
         for filename in "$home_dir/multiple_hypotheses_base/hypotheses_copy/$(method)/$(benchmark)"/*
-        echo new_run
-        echo $method
-        echo $benchmark
-        echo $filename
-        # julia --project=$home_dir $home_dir/multiple_hypotheses_base/scripts/compute_best_overall_program_rents.jl $method $benchmark $filename > "$home_dir/logs/$method_$benchmark_$filename.txt" &
-        sleep 10
+        do
+            echo new_run
+            echo $method
+            echo $benchmark
+            echo $filename
+            # julia --project=$home_dir $home_dir/multiple_hypotheses_base/scripts/compute_best_overall_program_rents.jl $method $benchmark $filename > "$home_dir/logs/$method_$benchmark_$filename.txt" &
+            sleep 10
+        done
     done
     # sleep 32400
 done
